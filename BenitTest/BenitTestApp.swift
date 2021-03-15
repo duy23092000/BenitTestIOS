c7258bc379368f71faf4667ab25bdf886f3c2b7b
Jayzer <duynguyen.benit@gmail.com>
2021-03-15 21:16:59 +0700

    Initial Commit

---
+++ BenitTest/BenitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenitTestApp.swift
+//  BenitTest
+//
+//  Created by Jayzer Nguyen on 15/3/21.
+//
+
+import SwiftUI
+
+@main
+struct BenitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BenitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BenitTest
+//
+//  Created by Jayzer Nguyen on 15/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
